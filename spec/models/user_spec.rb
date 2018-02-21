8df7c5c7287dd968ac8aced9aa9d96d34a69476d
Shen548 <shen548@gmail.com>
2018-02-21 13:45:25 -0700

    added factorygirl gem

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "Creation" do
     before do
-    @user = User.create( first_name: "asdf", last_name: "asdf", email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
-    end
-    
-    it "can be created" do
-      expect(@user).to be_valid
+    @user = User.create( first_name: "JON", last_name: "SNOW", email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
     end
 
-    it "cannot be created without first_name, last_name" do
-      @user.first_name =  nil
-      @user.last_name =  nil
-      expect(@user).to_not be_valid
+    describe "Creation" do
+      it "can be created" do
+        expect(@user).to be_valid
+      end
+
+      it "cannot be created without first_name, last_name" do
+        @user.first_name =  nil
+        @user.last_name =  nil
+        expect(@user).to_not be_valid
+      end
+    end
+
+  describe 'custom name methods' do
+    it 'has a full name method that combines first and last name' do
+    expect(@user.full_name).to eq("SNOW, JON")
     end
   end
 end
